9ef44336b5531f3fd9960798d95210fc823f9d4e
hanzlajhn <hanzlabhai123@gmail.com>
2024-02-14 09:19:32 +0500

    hanzla
    hanzla

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,8 @@ import 'package:get/get.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
